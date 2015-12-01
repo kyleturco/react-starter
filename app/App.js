d8a04373c19408175c325d6b820a4223eba0999a
Kyle Turco <kyleturco@gmail.com>
2015-11-30 18:47:28 -0600

    add beginning of todo app

--- app/App.js
+++ app/App.js
@@ -1,16 +1,30 @@
-import React, {Component} from 'react';
+import React, {createClass} from 'react';
 import {render} from 'react-dom';
 
-class App extends Component {
+const App = createClass({
+  getInitialState() {
+    return {
+      list: [],
+    };
+  },
+  handleSubmit(e) {
+    e.preventDefault();
+    console.log(this.refs.newItemForm[0].value);
+    this.refs.newItemForm[0].value = '';
+  },
   render() {
     return (
-      <div>
+      <div className='ui compact segment'>
         <h1>
-          Welcome to the react starter.
+          Todo List
         </h1>
+        <form ref='newItemForm' className='ui form' onSubmit={this.handleSubmit}>
+          <input placeholder='New Item' />
+          <button className='ui primary button'>Add Item</button>
+        </form>
       </div>
     );
   }
-}
+});
 
 render(<App />, document.getElementById('root'));
